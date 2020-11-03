171a4c909a6f7c3d868e04961140f8847c3ba2c6
bazhanov <bazhanov@stud.hs-heilbronn.de>
2020-11-03 22:56:46 +0100

    added logic for water measuring, also a swipe element to show absolute and relative water intake

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {SettingsService} from "../../service/settings.service";
+import {WaterMeasureService} from "../../service/water-measure.service";
+import {IonSlides} from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
@@ -7,20 +9,27 @@ import {SettingsService} from "../../service/settings.service";
   styleUrls: ['home.page.scss']
 })
 export class HomePage {
-  progressbarValue = 0.25;
-  progressbarValueInPercent: number;
+  @ViewChild('slides') slides: IonSlides;
+  slideOptions = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: true,
+    speed: 750,
+
+  }
   waterAdder = [100, 200, 500, 700, 1000, 1500];
 
-  constructor(public settingsService: SettingsService) {
-    this.getValueInPercent();
+  constructor(public settingsService: SettingsService,
+              public waterMeasureService: WaterMeasureService) {
+    this.waterMeasureService.getValueForProgBar();
   }
 
-  getValueInPercent() {
-    this.progressbarValueInPercent = this.progressbarValue * 100;
+  ionSlidesDidLoad() {
+    this.slides.startAutoplay();
   }
 
-  progressbarAbsolute() {
-
+  addWater(ml) {
+    this.waterMeasureService.addToProgressbar(ml)
   }
 
 }
--- src/app/service/water-measure.service.ts
+++ src/app/service/water-measure.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WaterMeasureService {
+  litresPerDay: number = 3000;
+  litresDrank: number = 0;
+  progBarValue: number;
+  progBarValueInPercent: number = 0;
 
   constructor() { }
+
+  addToProgressbar(addedWater) {
+    this.litresDrank = this.litresDrank + addedWater;
+    this.getValueForProgBar();
+    this.getValueInPercent();
+  }
+
+  getValueForProgBar() {
+    this.progBarValue = this.litresDrank / this.litresPerDay;
+  }
+
+  getValueInPercent() {
+    this.progBarValueInPercent = Math.round(this.progBarValue * 100);
+  }
+
 }
